67b7ade4afd2b678ac409c379e59ea2e1370538d
GaroleAtharva <72267124+GaroleAtharva@users.noreply.github.com>
2024-05-01 16:49:01 +0530

    Update pract3.cpp

--- pract3.cpp
+++ pract3.cpp
@@ -1,64 +1,82 @@
 #include <iostream>
-using namespace std;
-
-const int MAX_CHAPTERS = 100;
-const int MAX_SECTIONS = 100;
-const int MAX_SUBSECTIONS = 100;
 
 class Node {
 public:
-    char name[20];
-    int cnt;
-    Node* ptr[MAX_CHAPTERS];
+    char name[50];
+    Node* children[10];
+    int childCount;
 
-    Node() {
-        cnt = 0;
-        for(int i = 0; i < MAX_CHAPTERS; i++) {
-            ptr[i] = nullptr;
+    Node(const char* name) {
+        int i;
+        for (i = 0; name[i] != '\0'; i++) {
+            this->name[i] = name[i];
+        }
+        this->name[i] = '\0';
+        childCount = 0;
+    }
+
+    void addChild(Node* child) {
+        children[childCount++] = child;
+    }
+
+    void print(int level = 0) {
+        for(int i = 0; i < level; i++)
+            std::cout << " ";
+        std::cout << name << std::endl;
+        for(int i = 0; i < childCount; i++) {
+            children[i]->print(level + 1);
         }
     }
 };
 
-void create() {
-    Node* book = new Node;
-    cout << "Enter the name of the book: ";
-    cin >> book->name;
-    cout << "Enter the count of the chapters: ";
-    cin >> book->cnt;
-    for (int i = 0; i < book->cnt; i++) {
-        book->ptr[i] = new Node;
-        cout << "Enter the name of the chapter ("<<i+1<<") :";
-        cin >> book->ptr[i]->name;
-        cout << "Enter the count of the sections: ";
-        cin >> book->ptr[i]->cnt;
-        for (int j = 0; j < book->ptr[i]->cnt; j++) {
-            book->ptr[i]->ptr[j] = new Node;
-            cout << "Enter the name of section ("<<j+1<<") :";
-            cin >> book->ptr[i]->ptr[j]->name;
-            cout << "Enter the count of subsections: ";
-            cin >> book->ptr[i]->ptr[j]->cnt;
-            for (int k = 0; k < book->ptr[i]->ptr[j]->cnt; k++) {
-                book->ptr[i]->ptr[j]->ptr[k] = new Node;
-                cout << "Enter the name of subsection("<<k+1<<") :";
-                cin >> book->ptr[i]->ptr[j]->ptr[k]->name;
-            }
-        }
-    }
-    // Display the book structure
-    for (int i = 0; i < book->cnt; i++) {
-        cout << "\nChapter :"<<i+1<<" Name :"<<book->ptr[i]->name;
-        int seccnt=book->ptr[i]->cnt;
-        for (int j = 0; j < seccnt; j++) {
-            cout << "\nSection :"<<j+1<<" Name :"<<book->ptr[i]->ptr[j]->name;
-            int subseccnt=book->ptr[i]->ptr[j]->cnt;
-            for (int k = 0; k <subseccnt; k++) {
-                cout << "\nSubsection:"<<k+1<<" Name :"<<book->ptr[i]->ptr[j]->ptr[k]->name<<endl;
-            }
-        }
-    }
-}
-
 int main() {
-    create();
+    char bookName[50];
+    std::cout << "Enter the name of the book: ";
+    std::cin.getline(bookName, 50);
+    Node* book = new Node(bookName);
+
+    int numChapters;
+    std::cout << "Enter the number of chapters: ";
+    std::cin >> numChapters;
+    std::cin.ignore();
+
+    for(int i = 0; i < numChapters; i++) {
+        char chapterName[50];
+        std::cout << "Enter the name of chapter " << i+1 << ": ";
+        std::cin.getline(chapterName, 50);
+        Node* chapter = new Node(chapterName);
+
+        int numSections;
+        std::cout << "Enter the number of sections in chapter " << i+1 << ": ";
+        std::cin >> numSections;
+        std::cin.ignore();
+
+        for(int j = 0; j < numSections; j++) {
+            char sectionName[50];
+            std::cout << "Enter the name of section " << j+1 << " in chapter " << i+1 << ": ";
+            std::cin.getline(sectionName, 50);
+            Node* section = new Node(sectionName);
+
+            int numSubsections;
+            std::cout << "Enter the number of subsections in section " << j+1 << " of chapter " << i+1 << ": ";
+            std::cin >> numSubsections;
+            std::cin.ignore();
+
+            for(int k = 0; k < numSubsections; k++) {
+                char subsectionName[50];
+                std::cout << "Enter the name of subsection " << k+1 << " in section " << j+1 << " of chapter " << i+1 << ": ";
+                std::cin.getline(subsectionName, 50);
+                section->addChild(new Node(subsectionName));
+            }
+
+            chapter->addChild(section);
+        }
+
+        book->addChild(chapter);
+    }
+
+    book->print();  
+
     return 0;
 }
+    
